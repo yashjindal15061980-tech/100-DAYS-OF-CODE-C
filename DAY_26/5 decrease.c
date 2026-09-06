6eb45ab10eea3d04aa8ae4b0a788889e11d61339
yashjindal15061980-tech <yashjindal15061980@gmail.com>
2026-09-06 09:48:11 +0530

    DAY_26

---
+++ DAY_26/5 decrease.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 5; i >= 1; i--) {
+        for (int space = 1; space < i; space++) {
+            printf(" ");
+        }
+
+        for (int j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ DAY_26/increase decrease star.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("*\n");
+
+    for (int i = 0; i < 7; i++) {
+        printf("---\n");
+    }
+
+    printf("-\n");
+
+    return 0;
+}
