669e5398f54198229a6b8e2d4464ddddbcd96db8
yashjindal15061980-tech <yashjindal15061980@gmail.com>
2026-09-25 20:27:01 +0530

    Update count character string .c

--- DAY_41/count character string .c
+++ DAY_41/count character string .c
@@ -4,8 +4,6 @@ int main(void) {
     char str[1000];
     int count = 0;
     int ch;
-
-    /* Read one line, including spaces. */
     while ((ch = getchar()) != EOF && ch != '\n') {
         str[count++] = (char)ch;
     }
