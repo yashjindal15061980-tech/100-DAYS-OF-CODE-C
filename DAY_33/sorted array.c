e8169ab74efd138e8a84bc980bd195e9b17aec08
yashjindal15061980-tech <yashjindal15061980@gmail.com>
2026-09-25 20:07:55 +0530

    Update sorted array.c

--- DAY_33/sorted array.c
+++ DAY_33/sorted array.c
@@ -2,37 +2,29 @@
 
 int main()
 {
-    int n, i, element, pos;
+    int n, i, j, x;
+    int a[100];
 
     scanf("%d", &n);
 
-    int a[n + 1];
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &x);
+
+    i = n - 1;
+
+    while(i >= 0 && a[i] > x)
+    {
+        a[i + 1] = a[i];
+        i--;
+    }
+
+    a[i + 1] = x;
+    n++;
 
     for(i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-
-    scanf("%d", &element);
-
-    pos = 0;
-
-    while(pos < n && a[pos] < element)
-    {
-        pos++;
-    }
-
-    for(i = n; i > pos; i--)
-    {
-        a[i] = a[i - 1];
-    }
-
-    a[pos] = element;
-
-    for(i = 0; i <= n; i++)
-    {
         printf("%d ", a[i]);
-    }
 
     return 0;
 }
