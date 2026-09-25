a186456c944a7f3a1da68c738c8cc7631403aaa0
yashjindal15061980-tech <yashjindal15061980@gmail.com>
2026-09-25 19:37:17 +0530

    Refactor binary search function and variable names
    Refactor binary search implementation to use different variable names and structure.

--- DAY_33/binary search.c
+++ DAY_33/binary search.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 
-int main(void) {
-    int n, target;
-
+int main()
+{
+    int n, i, key;
+    int low, high, mid;
+    
     scanf("%d", &n);
 
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    int a[n];
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
     }
 
-    scanf("%d", &target);
+    scanf("%d", &key);
 
-    int left = 0;
-    int right = n - 1;
-    int found = -1;
+    low = 0;
+    high = n - 1;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
 
-        if (arr[mid] == target) {
-            found = mid;
-            break;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
+        if(a[mid] == key)
+        {
+            printf("Found at index %d", mid);
+            return 0;
+        }
+        else if(a[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
         }
     }
 
-    if (found != -1) {
-        printf("Found at index %d\n", found);
-    } else {
-        printf("-1\n");
-    }
+    printf("-1");
 
     return 0;
 }
